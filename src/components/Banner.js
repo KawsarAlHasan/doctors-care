dfc054d7b44ea2f4a4a4b0f927f0fc825859253e
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2023-06-07 16:50:19 +0600

    added banner infoCard Services

---
+++ src/components/Banner.js
@@ -0,0 +1,29 @@
+import React from "react";
+import chair from "../assets/images/chair.png";
+
+function Banner() {
+  return (
+    <div className="hero bannerBg">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img
+          src={chair}
+          className="rounded-lg lg:w-1/2 shadow-2xl"
+          alt="no inernet"
+        />
+        <div>
+          <h1 className="text-5xl font-bold">Box Office News!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn text-white btn-primary bg-gradient-to-r from-primary to-secondary">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Banner from "../../components/Banner";
+import InfoCards from "../../components/InfoCards";
+import Services from "../../components/Services";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="mx-5">
+      <Banner></Banner>
+      <InfoCards></InfoCards>
+      <Services></Services>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
