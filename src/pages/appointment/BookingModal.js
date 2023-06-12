a1e9f34906fda3044f0eb4b496d942e2faf8d3a4
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2023-06-13 02:23:44 +0600

    deploy firebase and connected vercel

--- src/pages/appointment/BookingModal.js
+++ src/pages/appointment/BookingModal.js
@@ -25,7 +25,7 @@ function BookingModal({ treatment, setTreatment, selectedDate, refetch }) {
       phone,
     };
 
-    fetch("http://localhost:5000/bookings", {
+    fetch("https://backend-theta-five.vercel.app/bookings", {
       method: "POST",
       headers: {
         "content-type": "application/json",
--- src/pages/login/Signup.js
+++ src/pages/login/Signup.js
@@ -47,7 +47,7 @@ function Signup() {
 
   const saveUser = (name, email) => {
     const user = { name, email };
-    fetch("http://localhost:5000/users", {
+    fetch("https://backend-theta-five.vercel.app/users", {
       method: "POST",
       headers: {
         "content-type": "application/json",
